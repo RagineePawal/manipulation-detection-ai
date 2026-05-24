3ff64c9023290c9aa5ad2fe9bd6e86355ada73a3
RagineePawal <ragineepawal2512@gmail.com>
2026-05-24 14:22:46 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,11 @@
+def predict_message(message):
+
+    result = {
+        "intent": "coercive",
+        "type": "guilt",
+        "domain": "workplace",
+        "severity": "medium",
+        "confidence": 0.88
+    }
+
+    return result
